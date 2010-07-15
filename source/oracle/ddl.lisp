0488b729f90c23128cdec2e3e21faf2d1f4f1813
David Lichteblau <david@knowledgetools.de>
2010-07-15 14:28:48 +0200

    rdbms: Optionally cache DDL queries
    ... needed because selects on Oracle's metadata tables are _extremely_
    slow, leading to many minutes of startup time otherwise
    
    ... the function to enable caching are available for all databases, but
    only the Oracle backend currently uses the cache

--- source/oracle/ddl.lisp
+++ source/oracle/ddl.lisp
@@ -6,6 +6,53 @@
 
 (in-package :hu.dwim.rdbms.oracle)
 
+(macrolet ((defcache (fun (&rest args) &body body)
+	     `(defmethod ,fun :around (,@args (database oracle))
+		(let ((tab (ddl-query-cache-of database)))
+		  (if tab
+		      ,(if body
+			   `(progn ,@body)
+			   `(symbol-macrolet
+				((cache (gethash ',fun tab :empty)))
+			      (if (eq cache :empty)
+				  (setf cache (call-next-method))
+				  cache)))
+		      (call-next-method))))))
+  (defcache database-list-sequences ())
+  (defcache database-list-tables ())
+  (defcache database-list-views ())
+  (defcache database-list-triggers ())
+  ;;
+  (defcache database-list-tables-and-columns ())
+  (defcache database-list-table-columns (name)
+    (mapcar #'cdr
+	    (remove name
+		    (database-list-tables-and-columns database)
+		    :key #'car
+		    :test-not #'equal)))
+  ;;
+  (defcache database-list-tables-and-indices ())
+  (defcache database-list-table-indices (name)
+    (mapcar #'cdr
+	    (remove name
+		    (database-list-tables-and-indices database)
+		    :key #'car
+		    :test-not #'equal)))
+  ;;
+  (defcache database-list-tables-and-primary-constraints ())
+  (defcache database-list-table-primary-constraints (name)
+    (mapcar #'cdr
+	    (remove name
+		    (database-list-tables-and-primary-constraints database)
+		    :key #'car
+		    :test-not #'equal)))
+  (defcache database-list-tables-and-foreign-keys ())
+  (defcache database-list-table-foreign-keys (name)
+    (remove name
+	    (database-list-tables-and-foreign-keys database)
+	    :key #'source-table-of
+	    :test-not #'equal)))
+
 ;; FIXME?  These functions return only object names accessible without
 ;; qualification by a schema name.  For use by Perec that is fine,
 ;; because Perec does not support access to more than one schema anyway.
@@ -50,6 +97,35 @@
 	    (database-effective-schema database))
     :result-type 'vector)))
 
+(def method database-list-tables-and-columns ((database oracle))
+  (map 'list
+   (lambda (column)
+     (cons (aref column 5)
+	   (make-instance 'sql-column
+			  :name (aref column 0)
+			  :type (sql-type-for-internal-type
+				 (aref column 1)
+				 (aref column 2)
+				 (aref column 3)
+				 (aref column 4)))))
+   (execute
+    (format nil "select column_name, data_type, char_length, data_precision, data_scale, table_name from all_tab_columns where owner='~A'"
+	    (database-effective-schema database))
+    :result-type 'vector)))
+
+(def method database-list-tables-and-indices ((database oracle))
+  (mapcar
+   (lambda (column)
+     (cons (third column)
+	   (make-instance 'sql-index
+			  :name (first column)
+			  :table-name (third column)
+			  :unique (equal "UNIQUE" (second column)))))
+   (execute
+    (format nil "select index_name, uniqueness, table_name from all_indexes where owner='~A'"
+	    (database-effective-schema database))
+    :result-type 'list)))
+
 (def method database-list-table-indices (name (database oracle))
   (mapcar
    (lambda (column)
@@ -60,6 +136,16 @@
    (execute
     (format nil "select index_name, uniqueness from all_indexes where table_name = '~A' and owner='~A'"
             name
+	    (database-effective-schema database))
+    :result-type 'list)))
+
+(def method database-list-tables-and-primary-constraints ((database oracle))
+  (mapcar
+   (lambda (column)
+     (cons (second column)
+	   (make-instance 'sql-constraint :name (first column))))
+   (execute
+    (format nil "select constraint_name, table_name from all_constraints where constraint_type='P' and owner='~A'"
 	    (database-effective-schema database))
     :result-type 'list)))
 
@@ -82,6 +168,39 @@
       (:set\ null :set-null)
       (:no\ action :restrict)
       (t (error "invalid action: ~A" str)))))
+
+(def method database-list-tables-and-foreign-keys ((database oracle))
+  (map 'list
+       (lambda (row)
+	 (make-instance 'foreign-key-descriptor
+			:name (elt row 0)
+			:source-table (elt row 1)
+			:source-column (elt row 2)
+			:target-table (elt row 3)
+			:target-column (elt row 4)
+			:delete-rule (sql-rule-name-to-lisp (elt row 5))
+			;; pretend that update and delete are the same:
+			:update-rule (sql-rule-name-to-lisp (elt row 5))))
+       (execute
+	(format nil "SELECT c3.constraint_name,
+                            c1.table_name, c1.column_name,
+                            c2.table_name, c2.column_name,
+                            c4.delete_rule
+                       FROM all_tab_columns c1
+                         JOIN all_cons_columns c3
+                           ON c1.table_name = c3.table_name
+                              AND c3.owner='~A'
+                              AND c1.column_name = c3.column_name
+                         JOIN all_constraints c4
+                           ON c3.constraint_name = c4.constraint_name
+                              AND c4.owner='~:*~A'
+                         JOIN all_cons_columns c2
+                           ON c4.r_constraint_name = c2.constraint_name
+                              AND c2.owner='~:*~A'
+                              AND c3.position = c2.position
+                     WHERE c1.owner='~:*~A'
+                           AND c4.constraint_type = 'R'"
+		(database-effective-schema database)))))
 
 (def method database-list-table-foreign-keys (table-name (database oracle))
   (map 'list
@@ -117,8 +236,3 @@
                            AND c4.constraint_type = 'R'"
 		(database-effective-schema database)
 		(string-downcase table-name)))))
-
-(defmethod oid-column-default-exists-in-database (node (database oracle))
-  (find (oid-column-default-trigger-name node database)
-	(database-list-triggers database)
-	:test #'string=))
