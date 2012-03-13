42367b6afb930246f99fa6962e1fe889da48984a
Tomas Hlavaty <tomas.hlavaty@knowledgetools.de>
2012-03-13 18:37:03 +0100

    introduce initialize-falloc-object and #+allegro system:memref-int optimizations

--- source/oracle/falloc.lisp
+++ source/oracle/falloc.lisp
@@ -57,14 +57,151 @@
           sb n))
   x)
 
+#+allegro
+(defun %initialize8 (base offset cnt val)
+  (declare (optimize (speed 3) (safety 0))
+           (fixnum offset cnt val))
+  (dotimes (i cnt)
+    (declare (fixnum i))
+    (macrolet ((mref (n)
+                 `(system:memref-int base offset ,n :unsigned-byte)))
+      (setf (mref i) val))))
+
+#+allegro
+(defun %initialize8vec (base offset cnt val)
+  (declare (optimize (speed 3) (safety 0))
+           (fixnum offset cnt))
+  (dotimes (i cnt)
+    (declare (fixnum i))
+    (let ((x (svref val i)))
+      (declare (fixnum x))
+      (macrolet ((mref (n)
+                   `(system:memref-int base offset ,n :unsigned-byte)))
+        (setf (mref i) x)))))
+
+#+allegro
+(defun %initialize16 (base offset cnt val)
+  (declare (optimize (speed 3) (safety 0))
+           (fixnum offset cnt val))
+  (let ((v0 (logand #xff val))
+        (v1 (logand #xff (setq val (ash val -8))))
+        (j -1))
+    (declare (fixnum v0 v1 j))
+    (dotimes (i cnt)
+      (declare (fixnum i))
+      (macrolet ((mref (n)
+                   `(system:memref-int base offset ,n :unsigned-byte)))
+        (setf (mref (incf j)) v0
+              (mref (incf j)) v1)))))
+
+#+allegro
+(defun %initialize32 (base offset cnt val)
+  (declare (optimize (speed 3) (safety 0))
+           (fixnum offset cnt))
+  (let ((v0 (logand #xff val))
+        (v1 (logand #xff (setq val (ash val -8))))
+        (v2 (logand #xff (setq val (ash val -8))))
+        (v3 (logand #xff (setq val (ash val -8))))
+        (j -1))
+    (declare (fixnum v0 v1 v2 v3 j))
+    (dotimes (i cnt)
+      (declare (fixnum i))
+      (macrolet ((mref (n)
+                   `(system:memref-int base offset ,n :unsigned-byte)))
+        (setf (mref (incf j)) v0
+              (mref (incf j)) v1
+              (mref (incf j)) v2
+              (mref (incf j)) v3)))))
+
+#+allegro
+(defun %initialize64 (base offset cnt val)
+  (declare (optimize (speed 3) (safety 0))
+           (fixnum offset cnt))
+  (let ((v0 (logand #xff val))
+        (v1 (logand #xff (setq val (ash val -8))))
+        (v2 (logand #xff (setq val (ash val -8))))
+        (v3 (logand #xff (setq val (ash val -8))))
+        (v4 (logand #xff (setq val (ash val -8))))
+        (v5 (logand #xff (setq val (ash val -8))))
+        (v6 (logand #xff (setq val (ash val -8))))
+        (v7 (logand #xff (setq val (ash val -8))))
+        (j -1))
+    (declare (fixnum v0 v1 v2 v3 v4 v5 v6 v7 j))
+    (dotimes (i cnt)
+      (declare (fixnum i))
+      (macrolet ((mref (n)
+                   `(system:memref-int base offset ,n :unsigned-byte)))
+        (setf (mref (incf j)) v0
+              (mref (incf j)) v1
+              (mref (incf j)) v2
+              (mref (incf j)) v3
+              (mref (incf j)) v4
+              (mref (incf j)) v5
+              (mref (incf j)) v6
+              (mref (incf j)) v7)))))
+
+(defun initialize-falloc-object (base offset cnt val type)
+  #+allegro
+  (declare (optimize (speed 3) (safety 0))
+           (fixnum offset cnt))
+  (let ((bufp (cffi-sys:inc-pointer base offset)))
+    (when val
+      #+allegro
+      (macrolet ((mref (i typ)
+                   `(system:memref-int base offset ,i ',typ :coerce))
+                 (one (i typ v)
+                   `(setf (mref ,i ,typ) ,v))
+                 (doop (w typ v)
+                   `(loop
+                       for i from 0 below cnt by ,w
+                       do (one i ,typ ,v)))
+                 (expand (w typ)
+                   `(cond
+                      ((vectorp val) (doop ,w ,typ (aref val i)))
+                      (t (doop ,w ,typ val)))))
+        (if (atom val)
+            (ecase type
+              (:double (expand 8 :double-float))
+              (:float (expand 4 :single-float))
+              ((oci:sb-1 :uint8 oci:ub-1)
+               (assert (typep val 'fixnum))
+               (%initialize8 base offset cnt val))
+              ((:short oci:sb-2 :unsigned-short oci:ub-2)
+               (assert (typep val 'fixnum))
+               (%initialize16 base offset cnt val))
+              ((:pointer oci:sb-4 oci:ub-4) (%initialize32 base offset cnt val))
+              (oci:oraub-8 (%initialize64 base offset cnt val)))
+            (ecase type
+              (oci:ub-1 (%initialize8vec base offset cnt val)))))
+      #-allegro
+      (macrolet ((one (i typ v)
+                   `(setf (cffi:mem-aref bufp ',typ ,i) ,v))
+                 (expand (typ)
+                   `(cond
+                      ((vectorp val) (dotimes (i cnt) (one i ,typ (aref val i))))
+                      (t (dotimes (i cnt) (one i ,typ val))))))
+        (ecase type
+          (:pointer (expand :pointer))
+          (:double (expand :double))
+          (:float (expand :float))
+          (:uint8 (expand :uint8))
+          (:unsigned-short (expand :unsigned-short))
+          (:short (expand :short))
+          (oci:sb-1 (expand oci:sb-1))
+          (oci:sb-2 (expand oci:sb-2))
+          (oci:sb-4 (expand oci:sb-4))
+          (oci:ub-1 (expand oci:ub-1))
+          (oci:ub-2 (expand oci:ub-2))
+          (oci:ub-4 (expand oci:ub-4))
+          (oci:oraub-8 (expand oci:oraub-8)))))
+    bufp))
+
 (defvar *falloc*)
 
-(defun call-with-falloc-object (nbytes1 count value heap fn value-fn)
-  (assert (and (plusp nbytes1) (plusp count)))
+(defun call-with-falloc-object (type nbytes1 count value heap fn)
+  (assert (and (positive-fixnum-p nbytes1) (positive-fixnum-p count)))
   (with-struct (falloc- base ht sb n maxh maxs gap) *falloc*
-    (macrolet ((p (x)
-                 `(cffi-sys:inc-pointer base ,x))
-               (check ()
+    (macrolet ((check ()
                  `(progn
                     (assert (<= 0 ht sb n))
                     (setq maxh (max maxh ht)
@@ -81,58 +218,25 @@
                   (y (setq ht (+ ht nbytes))))
               ;;(assert (zerop (mod ht 4)))
               (check)
-              (unwind-protect (funcall fn (funcall value-fn (p x) count value))
+              (unwind-protect
+                   (funcall fn
+                            (initialize-falloc-object base x count value type))
                 (check)
                 (assert (<= y ht))))
             (let ((x sb)
                   (y (setq sb (- sb nbytes))))
               ;;(assert (zerop (mod sb 4)))
               (check)
-              (unwind-protect (funcall fn (funcall value-fn (p y) count value))
+              (unwind-protect
+                   (funcall fn
+                            (initialize-falloc-object base y count value type))
                 (check)
                 (setq sb x)
                 (check))))))))
 
 (defmacro with-falloc-object ((var type &optional (count 1) value heap) &body body)
-  `(call-with-falloc-object
-    ,(cffi:foreign-type-size type) ,count ,value ,heap
-    (lambda (,var) ,@body)
-    (lambda (bufp cnt val)
-      (when val
-        #+allegro
-        ,(cond
-          ((eq 'oci:oraub-8 type)
-           `(cond
-              ((vectorp val)
-               (dotimes (i cnt)
-                 (let ((v (aref val i)))
-                   (setf (cffi:mem-aref bufp 'oci:ub-4 (* 2 i))
-                         (logand #xffffffff v)
-                         (cffi:mem-aref bufp 'oci:ub-4 (1+ (* 2 i)))
-                         (ash v -32)))))
-              (t
-               (dotimes (i cnt)
-                 (setf (cffi:mem-aref bufp 'oci:ub-4 (* 2 i))
-                       (logand #xffffffff val)
-                       (cffi:mem-aref bufp 'oci:ub-4 (1+ (* 2 i)))
-                       (ash val -32))))))
-          (t
-           `(cond
-              ((vectorp val)
-               (dotimes (i cnt)
-                 (setf (cffi:mem-aref bufp ',type i) (aref val i))))
-              (t
-               (dotimes (i cnt)
-                 (setf (cffi:mem-aref bufp ',type i) val))))))
-        #-allegro
-        (cond
-          ((vectorp val)
-           (dotimes (i cnt)
-             (setf (cffi:mem-aref bufp ',type i) (aref val i))))
-          (t
-           (dotimes (i cnt)
-             (setf (cffi:mem-aref bufp ',type i) val)))))
-      bufp)))
+  `(call-with-falloc-object ',type ,(cffi:foreign-type-size type) ,count ,value
+                            ,heap (lambda (,var) ,@body)))
 
 (defun call-with-falloc-string (string heap encoding null-terminated-p start end fn)
   ;; based on cffi::%foreign-string-alloc
@@ -145,11 +249,11 @@
            (count (funcall (cffi::octet-counter mapping) string start end 0))
            (tlen (cffi::null-terminator-len encoding))
            (length (if null-terminated-p (+ count tlen) count)))
-      (with-falloc-object (ptr :char length nil heap)
+      (with-falloc-object (ptr oci:ub-1 length nil heap)
         (funcall (cffi::encoder mapping) string start end ptr 0)
         (when null-terminated-p
           (dotimes (i tlen)
-            (setf (cffi:mem-ref ptr :char (+ count i)) 0)))
+            (setf (cffi:mem-ref ptr 'oci:ub-1 (+ count i)) 0)))
         (funcall fn ptr length)))))
 
 (defmacro with-falloc-string ((bufp-var len-var value &optional heap
